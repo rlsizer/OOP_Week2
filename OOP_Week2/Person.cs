893a20db45068f2c0366b6659bacfc7762650957
rlsizer <theryanlilo@gmail.com>
2017-04-07 11:58:58 -0400

    Added attributes and constructor methods for Person class.

--- OOP_Week2/Person.cs
+++ OOP_Week2/Person.cs
@@ -9,6 +9,31 @@ namespace OOP_Week2
     class Person
     {
 
+        //Class attributes/variables
+        public string name;
+        public int age;
+        public string hometown;
+        public string favFood;
+
+        //Default constructor method
+        public Person()
+        {
+
+        }
+
+        //Constructor method
+        //Keyword 'this' always refers to fields/attributes at top
+        public Person(string name, int age, string hometown)
+        {
+            this.name = name;
+            this.age = age;
+            this.hometown = hometown;
+            
+
+        }
+
+        
+
 
 
     }
